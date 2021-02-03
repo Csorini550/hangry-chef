4eb735922d35e7fa22850a6c475f58e11d844acb
Christopher Sorini <csorini13@gmail.com>
2021-02-02 22:14:49 -0600

    got kite because i hate google?

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,2 +1,3 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
+form .new_menue_form import NewMenueForm
--- app/forms/new_menue_form.py
+++ app/forms/new_menue_form.py
@@ -1,4 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, TextField, SubmitField, IntegerField, Form, FloatField, RadioField, SelectField
+
 from wtforms.validators import DataRequired, Email, ValidationError
-from app.models import User
+from app.models import Menue
+
+
+class NewMenueForm(FlaskForm):
+    user_id = Integer
