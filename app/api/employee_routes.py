672b7264c0832a3b2d5f8b6b1cbd83ea41d088bd
Christopher Sorini <csorini13@gmail.com>
2021-02-02 22:36:18 -0600

    finished form

---
+++ app/api/employee_routes.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.forms import NewEmployee
+from app.models import Employee
+from app.models import db
+
+emplyee_routes = Blueprint("employee", __name__)
+
+# GET ALL EMPLOYEES
+
+
+# GET EMPLOYEE BY ID
+
+
+# CREATE AN EMPLOYEE
+
+
+# DELETE AN EMPLOYEE
---
+++ app/forms/new_employee_form.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextField, SubmitField, IntegerField, Form, FloatField, RadioField, SelectField
+from app.models import Employee
+from wtforms.validators import DataRequired
+
+
+class NewEmployeeForm(FlaskForm):
+    user.id = IntegerField("user_id", validators=[DataRequired()])
+    first_name = StringField("first_name", validators=[DataRequired()])
+    last_name = StringField("last_name", validators=[DataRequired()])
+    salary = FloatField("salary", validators=[DataRequired()])
+    table_number = IntegerField("table_number", validators=[DataRequired()])
+    picture = StringField("picture")
