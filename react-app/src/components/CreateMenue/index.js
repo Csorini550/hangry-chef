cc33c01ad4e5e7b18a1a4ef5ef0008cf4e0d5378
Christopher Sorini <csorini13@gmail.com>
2021-02-08 11:32:39 -0600

    fixed some bugs in routes

--- react-app/src/components/CreateMenue/index.js
+++ react-app/src/components/CreateMenue/index.js
@@ -1,9 +1,50 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import { DragDropContext, Droppable, draggable } from 'react-beautiful-dnd'
+import { getFoodOrDrink, createFoodOrDrink } from '../../store/foodOrDrink'
+import { getIngredientsByUser } from '../../store/ingredient'
+import { createMenue, getMenueByUser } from '../../store/menue'
 
 const CreateMenue = () => {
+
+    const dispatch = useDispatch()
+    const { menueId } = useParams()
+    const loggedInUser = useSelector((state) => {
+        return state.session.user;
+    });
+    const userId = loggedInUser.id
+
+    const menueItem = useSelector((state) => {
+        return state.foodOrDrink;
+    })
+
+
+
+    useEffect(() => {
+        dispatch(getFoodOrDrink(menueId))
+        dispatch(getIngredientsByUser(userId))
+        dispatch(getMenueByUser(userId))
+        // dispatch(createFoodOrDrink(foodOrDrink))
+        // dispatch(createMenue(menueArray))
+    }, [])
+
+    // state = { menue: menue }
+    // const onDragEnd = (res) => {
+    //     const { destination, source, reason } = result;
+
+    //     if (!destination || reason === "CANCEL") {
+    //         return
+    //     }
+    //     if (destination.droppableId === source.droppableId && destination.index === source.index) {
+    //         return
+    //     }
+    //     const menue = Object.assign([], this.state.menue)
+    //     const droppedMenueItem = this.state.menue[source.index]
+    //     menue.splice(source.index, 1)
+    //     menue.splice(destination.index, 0, droppedMenueItem)
+    //     this.setState({ menue })
+    // }
 
     return (
         <>
@@ -12,6 +53,12 @@ const CreateMenue = () => {
             </div>
 
             <div>
+                <DragDropContext >
+
+                    <div>
+
+                    </div>
+                </DragDropContext>
                 <form>
 
                 </form>
